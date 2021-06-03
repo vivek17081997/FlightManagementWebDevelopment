3c28ae6e4d96b6bfd18504e86e95fe9d0d841328
vivekvermaeis <vivekvermaeis2019@gmail.com>
2021-06-03 11:07:20 +0530

    Created login Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ManageFlightComponent } from './components/manage-flight/manage-flight.component';
+import { SearchFlightComponent } from './components/search-flight/search-flight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ManageFlightComponent,
+    SearchFlightComponent
   ],
   imports: [
     BrowserModule,
